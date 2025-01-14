c1f10fd58a61fe43d719c5f6a00c2c9031493ba6
BEREKOUTOU <166011351+BEREKOUTOU@users.noreply.github.com>
2025-01-14 12:37:07 +0100

    buttonFilter

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,4 +1,4 @@
-// Récupération des fichiers JSON
+// Récupération des fichiers JSON works
 fetch('http://localhost:5678/api/works')
     .then(response => response.json())
     .then(data => {
@@ -6,68 +6,25 @@ fetch('http://localhost:5678/api/works')
         data.forEach(work => {  
             const figure = document.createElement('figure');
             figure.innerHTML = `
-                <img src="${work.imageUrl}" alt="${work.title}">
+                <img src="${work.imageUrl}" alt="${work.title}" data-category="${work.category}">
                 <figcaption>${work.title}</figcaption>
             `;
             document.getElementById('gallery').appendChild(figure);
         });
     })
-    
-    .catch(error => console.error(error));    
-
-
-//gestion des bouton tri
-const buttons = document.querySelectorAll('#myBtnContainer button');
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        buttons.forEach(btn => {
-            btn.classList.remove('active');
+    .catch(error => console.error(error)); 
+     // Récupération des fichiers JSON categories
+    fetch('http://localhost:5678/api/categories')
+    .then(response => response.json())
+    .then(data => {
+        const button = document.createElement('button');
+        button.innerText ='tous';
+        document.getElementById('myBtnContainer').appendChild(button);
+        // Affichage des travaux dans la galerie
+        data.forEach(category => {  
+            const button = document.createElement('button');
+            button.innerText = category.name;
+            document.getElementById('myBtnContainer').appendChild(button);
         });
-        button.classList.add('active');
-    });
-})
-// Fonction pour filtrer les éléments de la galerie
-const filterWorks = (category) => {
-    const works = document.querySelectorAll('figure');
-    works.forEach(work => {
-        const workCategory = work.getAttribute('data-category');
-        if (category === 'all' || workCategory === category) {
-            work.style.display = 'block';
-        } else {
-            work.style.display = 'none';
-        }
-    });
-}
- // Affichage du nombre de travaux correspondant au filtre
-  const tous = document.querySelector('.tous');
-  const objects = document.querySelector('.objects');
-  const appartements = document.querySelector('.appartements');
-  const hotelRestaurants = document.querySelector('.hôtelRestaurants');
-  // Ajout des écouteurs d'événements pour les filtres
-  const myBtnContainer = document.querySelector('.objects');
-  myBtnContainer.addEventListener('click', () => {
-    tous.classList.remove('active');
-    objects.classList.add('active');
-    const works = category.filterWorks (function (work) {
-        return work.category === 'objects';
     })
-   
-  });
-  myBtnContainer.addEventListener('click', () => {
-    tous.classList.remove('active');
-    appartements.classList.add('active');
-    const works = category.filterWorks (function (work) {
-        return work.category === 'appartements';
-    })
-  });
-  myBtnContainer.addEventListener('click', () => {
-    tous.classList.remove('active');
-    hotelRestaurants.classList.add('active');
-    const works = category.filterWorks (function (work) {
-        return work.category === 'hôtelRestaurants';
-    })
-  });
-  myBtnContainer.addEventListener('click', () => {
-    tous.classList.add('active');
-    filterWorks('all');
-  });
+    .catch(error => console.error(error)); 
