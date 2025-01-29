f493b9e04e4fe1d8b164442e6f7e8ecc74ee08be
BEREKOUTOU <166011351+BEREKOUTOU@users.noreply.github.com>
2025-01-29 16:50:56 +0100

    filter

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,59 +1,66 @@
+// Fonction des works
 // Récupération des fichiers JSON works
-fetch('http://localhost:5678/api/works')
-    .then(response => response.json())
-    .then(data => {
-        // Affichage des travaux dans la galerie
-        data.forEach(work => {  
-            const figure = document.createElement('figure');
-            figure.innerHTML = `
-                <img src="${work.imageUrl}" alt="${work.title}" data-category="${work.category}">
-                <figcaption>${work.title}</figcaption>
-            `;
-            document.getElementById('gallery').appendChild(figure);
-        });
-    })
-    .catch(error => console.error(error)); 
+const fetchWorks = async () => {
+    const data = await fetch('http://localhost:5678/api/works')
+        .then(response => response.json())
+        .catch(error => console.error(error));
+    return data;
+}
 
-// Récupération des fichiers JSON categories
-fetch('http://localhost:5678/api/categories')
-     .then(response => response.json())
-     .then(data => {
-         const button = document.createElement('button');
-         button.innerText = 'Tous';
-         button.dataset.category = 'tous';
-         document.getElementById('myBtnContainer').appendChild(button);
-         // Affichage des catégories dans les boutons de filtre
-         data.forEach(category => {  
-             const button = document.createElement('button');
-             button.innerText = category.name;
-             button.dataset.category = category.name.toLowerCase();
-             document.getElementById('myBtnContainer').appendChild(button);
-         });
-        
-         // Ajout d'un gestionnaire d'événement pour les boutons de filtre
-         const buttons = document.querySelectorAll('#myBtnContainer button');
-         buttons.forEach(button => {
-             button.classList.remove('active');
-             // Supprimer la classe active de tous les boutons et l'ajouter au courant
-             // Ajouter un gestionnaire d'événement de clic au bouton
-             button.addEventListener('click', () => {
-                 const activeButton = document.querySelector('#myBtnContainer button.active');
-                 if (activeButton) {
-                     activeButton.classList.remove('active');
-                 }
-                 const category = button.dataset.category;
-                 const works = document.querySelectorAll('#gallery figure');
-                 works.forEach(work => {
-                     if (category === 'tous' || work.dataset.category === category) {
-                         work.style.display = 'block';// afficher tous les travaux de la catégorie actuelle
-                     } else  {
-                         work.style.display = 'none'; // masquer tous les travaux autres que ceux de la catégorie actuelle  
-                     }
-                 });
-                 // Ajouter la classe active au bouton actuel
-                 button.classList.add('active');
-             });
-         });
-         console.log(buttons);
-     })
-     .catch(error => console.error(error));
+const displayWorks = async (category) => {
+    // Supprimé les works actuel
+    document.getElementById("gallery").innerHTML = "";
+
+    // Récupération des works
+    const works = await fetchWorks();
+
+    // Filtrage des works si une categorie existe
+    const filteredWorks = category ? works.filter((work) => work.category.name === category.name) : works;
+    filteredWorks.forEach(work => {
+        const figure = document.createElement('figure');
+        figure.innerHTML = `
+        <img src="${work.imageUrl}" alt="${work.title}" data-category="${work.category}">
+        <figcaption>${work.title}</figcaption>
+    `;
+        document.getElementById('gallery').appendChild(figure);
+    });
+}
+
+// Fonctions des catégories
+const fetchCategories = async () => {
+    const data = await fetch('http://localhost:5678/api/categories')
+        .then(response => response.json())
+        .catch(error => console.error(error));
+    return data;
+}
+
+const displayCategories = async () => {
+    const categories = await fetchCategories();
+
+    // Création du bouton "Tous"
+    createButtonCategory();
+
+    // Création des autres boutons
+    categories.forEach(category => {
+        createButtonCategory(category);
+    });
+}
+
+const createButtonCategory = (category) => {
+    const button = document.createElement('button');
+    button.textContent = category ? category.name : "Tous";
+    button.className = `category ${!category ? "active" : ""}`;
+    button.addEventListener("click", () => addButtonCategoryListener(button, category));
+    document.getElementById('myBtnContainer').appendChild(button);
+}
+
+const addButtonCategoryListener = (button, category) => {
+    const activeButton = document.querySelector(".category.active");
+    activeButton.classList.remove("active");
+    button.classList.add('active');
+    displayWorks(category)
+}
+
+// Initialisation
+displayWorks();
+displayCategories();
